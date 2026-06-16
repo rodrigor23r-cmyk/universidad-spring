7b963e0a5efc4b77b5f26043fd8ed474df2ef8bd
rodrigor23r-cmyk <rodrigor23r@gmail.com>
2026-06-16 13:48:12 +0200

    tras hacer empleado a medias

--- src/main/java/com/example/entities/Estudiante.java
+++ src/main/java/com/example/entities/Estudiante.java
@@ -1,8 +1,42 @@
 package com.example.entities;
 
+import java.io.Serializable;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
-public class Estudiante {
+@Table(name = "estudiantes")
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+public class Estudiante implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nombre;
+    private String primerApellido;
+    private String segundoApellido;
+
+    
+
+
+    
 
 }
