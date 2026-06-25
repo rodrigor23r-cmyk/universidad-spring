1c155db9a09fc3158c9e506b329797e9ddf62aed
rodrigor23r-cmyk <rodrigor23r@gmail.com>
2026-06-25 14:43:22 +0200

    fin clase jueves

--- src/main/java/com/example/controllers/EstudianteController.java
+++ src/main/java/com/example/controllers/EstudianteController.java
@@ -32,13 +32,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.bind.annotation.PostMapping;
 
-
-
 @Controller
 @RequestMapping("/estudiantes")
 @RequiredArgsConstructor
 public class EstudianteController {
-
 
     private final EstudianteService estudianteService;
     private final FacultadService facultadService;
@@ -56,34 +53,33 @@ public class EstudianteController {
 
     @GetMapping("/alta")
     public String mostrarFormularioAlta(Model model, @ModelAttribute Estudiante estudiante) {
-        
+
         model.addAttribute("facultades", facultadService.getAllFacultades());
 
         return "formularioAltaModificacion";
     }
-    
+
     @PostMapping("/persistir")
     public String procesarFormularioAltaModificacion(
-        Model model,
-        @Valid
-        @ModelAttribute Estudiante estudiante,
-        BindingResult result,
-        @RequestParam("numerosTelefono") String numerosTelefono,
-        @RequestParam("direccionesCorreo") String direccionesCorreo,
-        @RequestParam(name = "file", required = false) MultipartFile file) {
+            Model model,
+            @Valid @ModelAttribute Estudiante estudiante,
+            BindingResult result,
+            @RequestParam("numerosTelefono") String numerosTelefono,
+            @RequestParam("direccionesCorreo") String direccionesCorreo,
+            @RequestParam(name = "file", required = false) MultipartFile file) {
 
         if (result.hasErrors()) {
             model.addAttribute("facultades",
-            facultadService.getAllFacultades());
+                    facultadService.getAllFacultades());
 
             return "formularioAltaModificacion";
         }
 
-        if (file != null && !file.isEmpty()){
+        if (file != null && !file.isEmpty()) {
 
             Path rutaRelativa = Paths.get("src/main/resources/static/imagenes");
             String rutaAbsoluta = rutaRelativa.toFile().getAbsolutePath();
-            Path rutaCompleta = Paths.get(rutaAbsoluta+"/"+file.getOriginalFilename());
+            Path rutaCompleta = Paths.get(rutaAbsoluta + "/" + file.getOriginalFilename());
 
             try {
                 byte[] bytesFotoRecibida = file.getBytes();
@@ -93,6 +89,9 @@ public class EstudianteController {
                 e.printStackTrace();
             }
 
+        } else if (estudiante.getId() != 0) {
+            Estudiante estudianteAntiguo = estudianteService.getEstudianteById(estudiante.getId());
+            estudiante.setFoto(estudianteAntiguo.getFoto());
         }
 
         LOG.info("objeto estudiante recibido: ");
@@ -102,37 +101,38 @@ public class EstudianteController {
 
         if (!numerosTelefono.isEmpty() && !numerosTelefono.isBlank()) {
 
-            String [] arrayNumerosTelefono =numerosTelefono.split(";");
+            String[] arrayNumerosTelefono = numerosTelefono.split(";");
             List<String> listaNumerosTelefono = Arrays.asList(arrayNumerosTelefono);
             listaNumerosTelefono.forEach(num -> {
                 estudiante.getTelefonos().add(Telefono.builder().numero(num).estudiante(estudiante).build());
             });
         }
 
-      
         if (!direccionesCorreo.isEmpty() && !direccionesCorreo.isBlank()) {
-            String [] arrayDireccionesCorreo =direccionesCorreo.split(";");
+            String[] arrayDireccionesCorreo = direccionesCorreo.split(";");
             List<String> listaDireccionesCorreo = Arrays.asList(arrayDireccionesCorreo);
             listaDireccionesCorreo.forEach(dir -> {
                 estudiante.getCorreos().add(Correo.builder().email(dir).estudiante(estudiante).build());
             });
         }
 
-        // borrar 
-        if (telefonoService.existsByEstudiante(estudiante)){
-            telefonoService.deleteByEstudiante(estudiante);
+        // borrar
+        if (estudiante.getId() != 0) {
+            if (telefonoService.existsByEstudiante(estudiante)) {
+                telefonoService.deleteByEstudiante(estudiante);
+            }
+            if (correoService.existsByEstudiante(estudiante)) {
+                correoService.deleteByEstudiante(estudiante);
+            }
         }
-        if(correoService.existsByEstudiante(estudiante)){
-            correoService.deleteByEstudiante(estudiante);
-        }
-        
+
         estudianteService.saveEstudiante(estudiante);
         return "redirect:/estudiantes/listar";
     }
-    
+
     @GetMapping("/details/{id}")
     public String mostrarDetalles(Model model,
-        @PathVariable(name = "id", required = true) int estudiante_id){
+            @PathVariable(name = "id", required = true) int estudiante_id) {
 
         model.addAttribute("estudiante", estudianteService.getEstudianteById(estudiante_id));
 
@@ -141,33 +141,57 @@ public class EstudianteController {
 
     @GetMapping("/update/{id}")
     public String updateEstudiante(
-        Model model,
-        @PathVariable(name = "id", required = true) int estudiante_id) {
+            Model model,
+            @PathVariable(name = "id", required = true) int estudiante_id) {
 
         Estudiante estudiante = estudianteService.getEstudianteById(estudiante_id);
-        
+
         model.addAttribute("estudiante", estudiante);
         model.addAttribute("facultades", facultadService.getAllFacultades());
 
         Set<Telefono> telefonos = estudiante.getTelefonos();
-            if (telefonos.size() >0) {
-                    String numerosTelefono = telefonos.stream().map(telefono -> telefono.getNumero())
+        if (telefonos.size() > 0) {
+            String numerosTelefono = telefonos.stream().map(telefono -> telefono.getNumero())
                     .collect(Collectors.joining(";"));
-                    model.addAttribute("numerosTelefono", numerosTelefono);
-            }
+            model.addAttribute("numerosTelefono", numerosTelefono);
+        }
 
-            Set<Correo> correos = estudiante.getCorreos();
+        Set<Correo> correos = estudiante.getCorreos();
 
-            if(correos.size() > 0) {
+        if (correos.size() > 0) {
 
-                String direccionesCorreos =  correos.stream()
+            String direccionesCorreos = correos.stream()
                     .map(correo -> correo.getEmail())
                     .collect(Collectors.joining(";"));
-                model.addAttribute("direccionesCorreos", direccionesCorreos);
-            }
-
+            model.addAttribute("direccionesCorreos", direccionesCorreos);
+        }
 
         return "formularioAltaModificacion";
     }
+
+    @GetMapping("/delete/{id}")
+    public String deleteEstudiante(
+        Model model,
+        @PathVariable("id") int estudiante_id) {
+
+        Estudiante estudiante = estudianteService.getEstudianteById(estudiante_id);
+        String foto = estudiante.getFoto();
+        if (foto != null) {
+            Path rutaRelativa = Paths.get("src/main/resources/static/imagenes/" + foto);
+
+            if (Files.exists(rutaRelativa)){
+                try {
+                    Files.delete(rutaRelativa);
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+        estudianteService.deleteEstudiante(estudiante_id);
+
+        return "redirect:/estudiantes/listar";
+    }
     
+
 }
